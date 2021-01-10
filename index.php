db9530a1b13bd3208a61d1bee0a9bde1ca1720a5
Ishraq Hasan <ishraq10199@gmail.com>
2021-01-10 19:18:31 +0600

    Added temp

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <div class="container">
+        <h1> temporary change here </h1>
         <h1 class="jumbotron">TODO: landing page</h1>
         <h2>Sign in as a Patient with username: test123, and password: test123</h2>
         <a href="./static/sign_in.php"><button type=button class="btn btn-primary">Sign in</button></a>

da89d7f76548998d5273a868215cdc3d92ac0f29
Ishraq Hasan <ishraq10199@gmail.com>
2021-01-11 11:00:47 +0600

    AA

--- index.php
+++ index.php
@@ -1,34 +1,42 @@
-<?php
-include_once("./classes/db/DB_CONNECTION.class.php");
-$db = new DB_CONNECTION("medica_admin", "medica_admin");
-$stmt = $db->prepare("select * from tabs");
-$stmt->execute();
-while ($row = $stmt->fetch()) {
-    var_dump($row);
-}
-
-
-
-?>
-
-<html lang="en">
+<!DOCTYPE html>
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Medica</title>
-    <link rel="stylesheet" href="./static/assets/bootstrap/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>PatientDashboard_2</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
 </head>
 
 <body>
-    <div class="container">
-        <h1> temporary change here </h1>
-        <h1 class="jumbotron">TODO: landing page</h1>
-        <h2>Sign in as a Patient with username: test123, and password: test123</h2>
-        <a href="./static/sign_in.php"><button type=button class="btn btn-primary">Sign in</button></a>
-
+    <div>
+        <div class="header-blue">
+            <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+                <div class="container-fluid"><a class="navbar-brand" href="#">Project Medica</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse"
+                        id="navcol-1">
+                        <form class="form-inline mr-auto" target="_self">
+                            <div class="form-group"><input class="form-control search-field" type="search" id="search-field" name="search"></div>
+                        </form><span class="navbar-text"> <a class="login" href="#">Log In</a></span></div>
+                </div>
+            </nav>
+            <div class="container hero">
+                <div class="row">
+                    <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
+                        <h1>Medical care, simplified</h1>
+                        <p>Project Medica aims to simplify your medical needs, making the hassle of creating appointments and managing healthcare history a thing of the past</p><button class="btn btn-light btn-lg action-button" type="button">Learn More</button></div>
+                    <div
+                        class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder"><img src="assets/img/iphone_with_medica_logo.png" style="width: 232px;height: 463px;">
+                        <div class="iphone-mockup"></div>
+                </div>
+            </div>
+        </div>
     </div>
-
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>

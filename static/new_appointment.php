43d77216471862abcfcad461c076381130d79691
Ishraq Hasan <ishraq10199@gmail.com>
2021-01-19 02:25:41 +0600

    Added flow diagram

--- static/new_appointment.php
+++ static/new_appointment.php
@@ -10,7 +10,7 @@ include "./helpers/Patient/doctor_search_result_cards.inc.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>PatientDashboard_2</title>
+    <title>New Appointment</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
     <link rel="stylesheet" href="assets/css/styles.min.css">
